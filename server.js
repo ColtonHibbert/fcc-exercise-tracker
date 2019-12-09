cd94e79aabe7039f51efc2b8bca379c82b3c3410
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2019-12-09 11:56:43 -0700

    first endpoint

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const knex = require('knex');
 
 const cors = require('cors')
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+//const mongoose = require('mongoose')
+//mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
 
 app.use(cors())
 
+const db = knex({
+  client: "pg",
+  connection: {
+    host: '127.0.0.1',
+    database: 'fccexercisetracker'
+  }
+})
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(express.urlencoded())
@@ -20,28 +28,36 @@ app.get('/', (req, res) => {
 
 
 // Not found middleware
-app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
+// app.use((req, res, next) => {
+//   return next({status: 404, message: 'not found'})
+// })
+
+// // Error Handling middleware
+// app.use((err, req, res, next) => {
+//   let errCode, errMessage
+
+//   if (err.errors) {
+//     // mongoose validation error
+//     errCode = 400 // bad request
+//     const keys = Object.keys(err.errors)
+//     // report the first validation error
+//     errMessage = err.errors[keys[0]].message
+//   } else {
+//     // generic or custom error
+//     errCode = err.status || 500
+//     errMessage = err.message || 'Internal Server Error'
+//   }
+//   res.status(errCode).type('txt')
+//     .send(errMessage)
+// })
+
+app.post("/api/exercise/user", function (req, res) {
+  console.log("new-user was pinged")
+  const username = req.body.username
+  console.log(username);
+  
 })
 
-// Error Handling middleware
-app.use((err, req, res, next) => {
-  let errCode, errMessage
-
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message
-  } else {
-    // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
-  }
-  res.status(errCode).type('txt')
-    .send(errMessage)
-})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
